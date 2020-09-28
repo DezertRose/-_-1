28f920128f2f0d442c3555339d6102851e59095e
DezertRose <57520792+DezertRose@users.noreply.github.com>
2020-09-28 11:17:19 +0300

    Update Part1.js

--- Part1.js
+++ Part1.js
@@ -1,30 +1,31 @@
 function first(from){
-let arr=[];
-for(i=0;i<from; i++){
-arr[i]=i;
-}
-console.log(arr);
-return arr;
+ let arr=[];
+    for(i=0;i<from; i++){
+    arr[i]=i;
+    }
+ console.log(arr);
+ return arr;
 } 
 
 a=first(5);
 
 function second(one, two, tree){
-if(one%two===0 && one%tree===0){
-console.log(true);
-return true;
-}
-else{
-console.log(false);
-return false;}
+ if(one%two===0 && one%tree===0){
+   console.log(true);
+   return true;
+ }
+ else{
+  console.log(false);
+  return false;
+ }
 } 
 
 b=second(12, 6, 3);
 
 function сapitalize(str){
-if(!str){return str;}
-console.log(str[0].toUpperCase()+str.slice(1));
-return str[0].toUpperCase()+str.slice(1);
+  if(!str){return str;}
+  console.log(str[0].toUpperCase()+str.slice(1));
+  return str[0].toUpperCase()+str.slice(1);
 }
 
 str=сapitalize('name');
